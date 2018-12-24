1eb5d162791705733cd3e395b029c840fa45f885
Daniil Borovkov <daniilborowkow@gmail.com>
2018-12-24 22:01:13 +0600

    Initial commit

---
+++ db-promo-on-register.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     DB register on popup
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Register on popup
+ * Author:          Daniil Borovkov
+ * Author URI:      https://daniilborovkov.site
+ * Text Domain:     db-promo-on-register
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Db_Promo_On_Register
+ */
+
+// Your code starts here.
---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Db_Promo_On_Register
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
